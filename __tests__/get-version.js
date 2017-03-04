4c8c10bb9955601562c66f4724a3305881945c7a
Bu Kinoshita <bukinoshita@gmail.com>
2017-03-04 13:10:36 -0500

    Add tests

---
+++ __tests__/get-version.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import test from 'ava'
+import getVersion from './../lib/get-version'
+
+test('should return the version passed', t => {
+  const res = getVersion('4')
+  t.is(res, '4')
+})
+
+test('should return the version 6', t => {
+  const res = getVersion()
+  t.is(res, 6)
+})
