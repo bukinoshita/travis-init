586ad8ed112803a6577d0a7a7f319bfa61649ab8
Bu Kinoshita <bukinoshita@gmail.com>
2017-03-06 14:52:06 -0500

    Test

--- __tests__/file-template.js
+++ __tests__/file-template.js
@@ -13,3 +13,29 @@ node_js:
 
   t.is(res, expected)
 })
+
+test('should return the file template with more than one version', t => {
+  const res = fileTemplate('node_js', ['4', '5', '6'])
+  const expected = (
+`language: node_js
+node_js:
+  - 4
+  - 5
+  - 6
+`)
+
+  t.is(res, expected)
+})
+
+test('should return an error', t => {
+  const res = fileTemplate('node_js', ['4', '9', '6'])
+  const expected = (
+`language: node_js
+node_js:
+  - 4
+  - 9
+  - 6
+`)
+
+  t.is(res, expected)
+})
